3b337fc2bb60c24b6c3d10598677af223d5d43f0
unknown <ivanpintos877@gmail.com>
2024-04-03 08:29:37 -0300

    Clase 1

---
+++ index.js
@@ -0,0 +1,10 @@
+let x = null;
+let y = null;
+
+function suma(x, y) {
+    x = document.getElementsById("value1").value;
+    y = document.getElementById("value2").value;
+
+    z = x + y;
+    return console.log(z)
+}
